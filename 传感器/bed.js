04590b81bd765272a211429ac3689fa77b9096de
shantidong <shantidongkor@163.com>
2017-06-05 14:14:26 +0900

    Update bed.js

--- 传感器/bed.js
+++ 传感器/bed.js
@@ -4,21 +4,21 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
+bed.set('views', __dirname + '/views');
+bed.set('view engine', 'ejs')
+bed.use(express.static(__dirname + '/public'))
 
 var DHT11_GPIO = 21;   //GPIO端口
 var DHT11 = 11;              //传感器型号
 sensorLib.initialize(DHT11, DHT11_GPIO);
 
-app.get('/interface', function(req, res){
+bed.get('/interface', function(req, res){
     var readout = sensorLib.read();
 //    console.dir(readout)
     res.render('interface', {temperature: readout.temperature.toFixed(2), humidity: readout.humidity.toFixed(2)});
 });
 
-var server = app.listen(3000, function() {
+var server = bed.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 interface.js
